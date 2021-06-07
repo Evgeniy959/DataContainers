f75a79c767d09ef6296a5f7f89a02eaf4cece87e
Pili_zr36 <Pili_zr36@itstepru.onmicrosoft.com>
2021-06-08 02:32:52 +0700

    List2

---
+++ Home work 9 OOP (List2)/main.cpp
@@ -0,0 +1,92 @@
+﻿/*TODO:
+В двусвязный список добавить методы :
+1. void push_back(int Data);
+2. void pop_front();
+3. void pop_back();
+4. void insert(int index, int data);
+5. void erase(int index);
+6. Деструктор должен очищать список;
+7. Этот проверочный код должен заработать :
+List list = { 3,5,8,13,21 };
+for (int i : list)
+   cout << i << tab;
+cout << endl;*/
+
+#include<iostream>
+using namespace std;
+
+#define tab "\t"
+
+class List
+{
+	class Element
+	{
+		int Data;
+		Element* pNext;
+		Element* pPrev;
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr)
+		{
+			this->Data = Data;
+			this->pNext = pNext;
+			this->pPrev = pPrev;
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class List;
+	}*Head, *Tail;	//Сразу же после описания класса объявляем два указателя на объекты этого класса - указатели на начальный и конечный элемент
+	/*Element *Head;
+	Element *Tail;*/
+	unsigned int size;
+public:
+	List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~List()
+	{
+		cout << "LDestructor:\t" << this << endl;
+	}
+
+	//			Adding elements:
+	void push_front(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr)
+		{
+			Head = Tail = new Element(Data);
+			size++;
+			return;
+		}
+		Element* New = new Element(Data);
+		New->pNext = Head;
+		Head->pPrev = New;
+		Head = New;
+		size++;
+	}
+
+	//			Methods:
+	void print()
+	{
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp << tab << Temp->pPrev << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Количество элементов списка: " << size << endl;
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_front(rand() % 100);
+	}
+	list.print();
+}
